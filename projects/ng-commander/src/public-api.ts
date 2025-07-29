6adace05a0d4cad2c0d142277b9d9fc57b767ddd
Hadrien Delphin <delphin.hadrien@gmail.com>
2025-07-29 16:26:35 +0200

    Bump version to 1.1.1 and export CommandEvent interface
    - Update version in package.json to 1.1.1
    - Add export for CommandEvent interface in public-api.ts

--- projects/ng-commander/src/public-api.ts
+++ projects/ng-commander/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './Application/commander.service';
 export * from './Application/commander.service.configuration';
+export * from './Domain/command-event.interface';
 export * from './Domain/command.interface';
